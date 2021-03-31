77ec0ede687a65f4ed561c47ecb5c4b66f556f8a
moong <moonki0623@naver.com>
2021-03-31 23:13:27 +0900

    [branch] : making new branch

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import HomeScreen from './screens/#1_auth/auth';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen></HomeScreen>
+       <HomeScreen></HomeScreen>
     </View>
   );
 }
