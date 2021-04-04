2e2ed01fa8f12c82fb2a4db1c463ff33787e8eb6
Seungwoo Kim <sonicdx886@gmail.com>
2021-04-04 17:18:57 +0900

    [etc]: session 진행중

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import HomeScreen from './screens/#1_auth/signUp';
+import HomeScreen from './screens/#4_individual/indiv_session';
 
 const App = () => {
   return (
---
+++ src/screens/#4_individual/indiv_session.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { View } from 'react-native';
+import GrayTextButton from '../../components/GrayTextButton';
+import UnderLinedTextInput from '../../components/UnderlinedTextInput';
+import { Spacing } from '../../styles';
+import GreenButton from '../../components/GreenButton';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+const LogoContainer = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+`
+
+const AuthContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+`
+
+const LogoImage = styled.Image`
+  width: ${Spacing.SCALE_200};
+  height: ${Spacing.SCALE_200};
+`
+
+const IndivSession = () => {
+  const [loginText, setLoginText] = useState('');
+  const [singupText, setSignupText] = useState('');
+  return (
+  <Container>
+    <LogoContainer>
+      <LogoImage source={require('../../assets/logo_square.png')} />
+    </LogoContainer>
+    <AuthContainer>
+      <UnderLinedTextInput placeholder={'아이디'}  value={loginText} onChangeText={setLoginText} />
+      <UnderLinedTextInput placeholder={'비밀번호'} value={singupText} onChangeText={setSignupText} secureTextEntry={true} />
+      <GreenButton content={'로그인'} />
+      <View style={{alignSelf:'flex-end'}}>
+        <GrayTextButton content='회원가입' />
+      </View>
+    </AuthContainer>
+  </Container>
+)}
+
+const Stack = createStackNavigator()
+
+export default () => {
+  return(
+    <IndivSession />
+  )
+}
