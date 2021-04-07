e8dbdfc2f6f6ff72e66491f1469d8de73646b982
nowkim <peterhyunjae@naver.com>
2021-04-07 16:34:49 +0900

    .

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Footer_dash from './components/Footer_dash';
-import IndividualSession from './screens/#4_individual/indiv_session';
+import IndividualSession from './src/screens/#4_individual/indiv_session';
 
 
 
