dfc3a8a1471525dc3b084f5c0c03faf0f0fd0c2d
Seungwoo Kim <sonicdx886@gmail.com>
2021-04-09 22:27:51 +0900

    [dep]: react-native-webview 재설치 (expo install...)
    DA-16

--- src/screens/#5_profile/change_view.js
+++ src/screens/#5_profile/change_view.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, Text, Button, Image, StyleSheet } from 'react-native';
 import { Spacing, Typography, Colors } from '../../styles';
+import { ECharts } from "react-native-echarts-wrapper";
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -31,12 +32,66 @@ const styles = StyleSheet.create({
     justifyContent: 'space-evenly',
   },
   graphContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    width: '100%',
   }
 })
 
 function Change_view({ navigation }) {
+
+  // ---------------------------- chart test start ----------------------------
+
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["4/1", "4/2", "4/3", "4/4", "4/5", "4/6", "4/7"]
+    },
+    yAxis: {
+      type: "value"
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "line"
+      },
+      {
+        data: [320, 432, 401, 334, 290, 330, 520],
+        type: "line"
+      }
+    ]
+  }
+
+  const additionalCode = `
+    chart.on('click', function(param) {
+        var obj = {
+        type: 'event_clicked',
+        data: param.data
+        };
+
+        sendData(JSON.stringify(obj));
+    });
+  `
+
+  const onData = param => {
+    const obj = JSON.parse(param);
+
+    if (obj.type === "event_clicked") {
+      alert(`you tapped the chart series: ${obj.data}`);
+    }
+  }
+
+  const onRef = ref => {
+    if (ref) {
+      this.chart = ref;
+    }
+  }
+
+  const onButtonClearPressed = () => {
+    this.chart.clear();
+  };
+
+  // ---------------------------- chart test end ------------------------------
+
     return (
       <View style={styles.mainContainer}>
         <Text style={styles.title}>김승우 고객님 변화보기</Text>
@@ -56,8 +111,18 @@ function Change_view({ navigation }) {
         <View style={styles.subTitleContainer}>
           <Text style={styles.subTitle}>Graph</Text>
         </View>
-        <View style={styles.graphContainer}>
+        <View style={styles.graphContainer}>``
           <Text>This is graph</Text>
+          <Button title='Clear' onPress={onButtonClearPressed} />
+          <ECharts
+            ref={onRef}
+            option={option}
+            additionalCode={additionalCode}
+            onData={onData}
+            onLoadEnd={() => {
+              this.chart.setBackgroundColor(Colors.GRAY_LIGHT);
+            }}
+          />
         </View>
       </View>
     );
