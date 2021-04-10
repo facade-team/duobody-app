1c641a221c88f8f53d14ae2a55afd19533a8911b
Choieeh <chyh0182@naver.com>
2021-04-10 23:29:10 +0900

    [fix]: edit bug fixed

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import Dash_dash from './src/screens/#2_dashboard/dashboard';
 import IndividualSession from './src/screens/#4_individual/indiv_session';
 import Search from './src/screens/#3_mem_admin/mem_search';
 import Add from './src/screens/#3_mem_admin/mem_add';
+import Edit from './src/screens/#3_mem_admin/mem_edit';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Add></Add>
+      <Edit></Edit>
     </View>
   );
 }
--- src/screens/#3_mem_admin/mem_edit.js
+++ src/screens/#3_mem_admin/mem_edit.js
@@ -19,20 +19,20 @@ const AddContainer = styled.View`
 
 
 export default () => {
-  const [Name, setName] = useState('')
-  const [Hp, setHp] = useState('')
-  const [Address, setAddress] = useState('')
-  const [Age, setAge] = useState('')
-  const [Height, setHeight] = useState('')
+  const [Name, setName] = useState('김ㅇㅇ')
+  const [Hp, setHp] = useState('010-1234-5678')
+  const [Address, setAddress] = useState('서울시 강남구')
+  const [Age, setAge] = useState('24세')
+  const [Height, setHeight] = useState('178cm')
   return (
   <Container>
     
     <AddContainer>
       <UnderLinedTextInput placeholder={'이름'}  value={Name} onChangeText={setName} />
       <UnderLinedTextInput placeholder={'H.P'}  value={Hp} onChangeText={setHp} />
-      <UnderLinedTextInput placeholder={'주소'} value={Address} onChangeText={setAddress} secureTextEntry={true} />
-      <UnderLinedTextInput placeholder={'나이'} value={Age} onChangeText={setAge} secureTextEntry={true} />
-      <UnderLinedTextInput placeholder={'키'} value={Height} onChangeText={setHeight} secureTextEntry={true} />
+      <UnderLinedTextInput placeholder={'주소'} value={Address} onChangeText={setAddress}/>
+      <UnderLinedTextInput placeholder={'나이'} value={Age} onChangeText={setAge}/>
+      <UnderLinedTextInput placeholder={'키'} value={Height} onChangeText={setHeight} />
       <GreenButton content={'완료하기'} />
     </AddContainer>
   </Container>
