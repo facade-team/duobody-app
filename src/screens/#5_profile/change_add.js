b87e21edbf5b58cc193dc451cbfc7376f5f7cc73
Seungwoo Kim <sonicdx886@gmail.com>
2021-04-21 15:52:56 +0900

    [feat]: expo image picker 테스트중

--- src/screens/#5_profile/change_add.js
+++ src/screens/#5_profile/change_add.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, Dimensions, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, Dimensions, TouchableOpacity, Image } from 'react-native';
 import { Colors, Spacing, Typography } from '../../styles';
 import UnderLinedTextInputBig from '../../components/UnderlinedTextInputBig';
 import axios from '../../axios/api';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import getDateStringWithNumber from '../../utils/getDateStringWithNumber';
+
+import * as ImagePicker from 'expo-image-picker';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 const change_add = ({navigation}) => {
@@ -17,6 +20,27 @@ const change_add = ({navigation}) => {
   const [pickedDate, setPickedDate] = useState('2021-04-10');
   const [CalDate, setCalDate] = useState(new Date(pickedDate))
   const [InbodyFromDB, setInbodyFromDB] = useState([]);
+
+  // 승우가 짠 부분 start
+  const [image, setImage] = useState(null);
+  
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  }
+
+
+  // 승우가 짠 부분 end
 
   const onChangePickedDate = (event, selectedDate) => {
     const currentDate = selectedDate;
@@ -42,6 +66,16 @@ const change_add = ({navigation}) => {
   };
 
   useEffect(()=>{
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        console.log(status)
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+
     if(!gotData || FormerID !== _id){
       axios.get(`/trainee/${_id}`)
       .then(res =>{
@@ -73,69 +107,76 @@ const change_add = ({navigation}) => {
 
 
 return (
-  <View style = {styles.container}>
-    <View style = {styles.maincontainer}>
-      <View style = {styles.name}>
-        <Text style = {styles.nametext}>
-          {DATAFromDB.name} 회원님
-        </Text>
-        <TouchableOpacity
-          onPressOut={getApiTest}>
-        <Text>API Test</Text>
-      </TouchableOpacity>
-      </View>
-      
-      <View style = {styles.dayselect}>
-        <DateTimePicker
-          style = {{width: Spacing.SCALE_80}}
-          testID="dateTimePicker"
-          value={CalDate}
-          mode={'date'}
-          display="default"
-          onChange={onChangePickedDate}
-        />
-
-      </View>
-
-      <View style = {styles.wbmfcontainer}>
-        <View style = {styles.infoinput}>
-          <View style = {styles.individual}>
-            <UnderLinedTextInputBig placeholder={'몸무게: ??kg'} value = {weightText} onChangeText={setWeightText}/>
-          </View>
-          <View style = {styles.individual}>
-            <UnderLinedTextInputBig placeholder={'BMI: ??kg/m²'} value = {BMIText} onChangeText={setBMIText}/>
-          </View>
-          <View style = {styles.individual}>
-            <UnderLinedTextInputBig placeholder={'골격근: ??kg'} value = {muscleText} onChangeText={setMuscleText}/>
-          </View>
-          <View style = {styles.individual}>
-            <UnderLinedTextInputBig placeholder={'체지방: ??kg'} value = {fatText} onChangeText={setFatText}/>
-          </View>
-          <Text>{InbodyFromDB.bmi}</Text>
-        </View>
-      </View>
-
-      <TouchableOpacity 
-        style = {styles.addexbody}
-        onPress = {()=> navigation.navigate('Change_View')}>
-        <View>
-          <Text style = {styles.greenbutton}>
-            Ex-Body 추가
+  <ScrollView>
+    <View style = {styles.container}>
+      <View style = {styles.maincontainer}>
+        <View style = {styles.name}>
+          <Text style = {styles.nametext}>
+            {DATAFromDB.name} 회원님
           </Text>
+          <TouchableOpacity
+            onPressOut={getApiTest}>
+          <Text>API Test</Text>
+        </TouchableOpacity>
         </View>
-      </TouchableOpacity>
+        
+        <View style = {styles.dayselect}>
+          <DateTimePicker
+            style = {{width: Spacing.SCALE_80}}
+            testID="dateTimePicker"
+            value={CalDate}
+            mode={'date'}
+            display="default"
+            onChange={onChangePickedDate}
+          />
 
-      <TouchableOpacity 
-        style = {styles.addexbody_2}
-        onPress = {()=> navigation.goBack()}>
-        <View>
-          <Text style = {styles.greenbutton}>
-            확인
-          </Text>
         </View>
-      </TouchableOpacity>
+
+        <View style = {styles.wbmfcontainer}>
+          <View style = {styles.infoinput}>
+            <View style = {styles.individual}>
+              <UnderLinedTextInputBig placeholder={'몸무게: ??kg'} value = {weightText} onChangeText={setWeightText}/>
+            </View>
+            <View style = {styles.individual}>
+              <UnderLinedTextInputBig placeholder={'BMI: ??kg/m²'} value = {BMIText} onChangeText={setBMIText}/>
+            </View>
+            <View style = {styles.individual}>
+              <UnderLinedTextInputBig placeholder={'골격근: ??kg'} value = {muscleText} onChangeText={setMuscleText}/>
+            </View>
+            <View style = {styles.individual}>
+              <UnderLinedTextInputBig placeholder={'체지방: ??kg'} value = {fatText} onChangeText={setFatText}/>
+            </View>
+            <Text>{InbodyFromDB.bmi}</Text>
+          </View>
+        </View>
+
+        {image && (
+          <View style={{alignSelf: 'center'}}>
+            <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+          </View>
+        )}
+        <TouchableOpacity 
+          style = {styles.addexbody}
+          onPressOut = {()=> pickImage()}>
+          <View>
+            <Text style = {styles.greenbutton}>
+              Ex-Body 추가
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style = {styles.addexbody_2}
+          onPress = {()=> navigation.goBack()}>
+          <View>
+            <Text style = {styles.greenbutton}>
+              확인
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
+  </ScrollView>
 )}
 
 
@@ -146,12 +187,14 @@ const styles = StyleSheet.create({
     flex: 2,
     backgroundColor: Colors.PRIMARY,
     alignItems: 'center',
+    justifyContent: 'center'
   },
   maincontainer: {
     flex: 2,
     backgroundColor: Colors.WHITE,
     borderRadius: 10,
     width: '98%',
+    justifyContent: 'center',
   },
   name: {
     paddingVertical: Spacing.SCALE_36,
